31a415c4fc452b1e30930353b92448d0b54f12dd
Guzel Nasybullina <guzelka.nasybullina@gmail.com>
2025-09-16 22:21:43 +0300

    add func for sort

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 
 def filter_by_state(list_of_dict: list, state: str = 'EXECUTED') -> list:
     """Функция фильтрует список словарей по значению ключа"""
@@ -6,3 +8,9 @@ def filter_by_state(list_of_dict: list, state: str = 'EXECUTED') -> list:
         if dict_item.get('state') == state:
             filtered_list.append(dict_item)
     return filtered_list
+
+
+def sort_by_date(data_list: list, data_key = 'date', descending=True) -> list:
+    """Функция сортирует список словарей по дате в заданном по умолчанию порядке сортировки - по убыванию"""
+    data_key = datetime.strptime('2023-10-22', '%Y-%m-%d')
+    return sorted(data_list, key=lambda x: datetime.strptime(x[data_key], '%Y-%m-%dT%H:%M:%S.%f'), reverse=descending)
