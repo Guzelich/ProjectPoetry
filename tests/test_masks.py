f84d3a3ca4229f6870ea52a635d2602b61a95719
Guzel Nasybullina <guzelka.nasybullina@gmail.com>
2025-10-04 16:43:32 +0300

    testing get_mask_card_num

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -12,3 +12,11 @@ def test_get_mask_account_negative(invalid_input):
     assert get_mask_account(invalid_input)
 
 
+@pytest.mark.parametrize('card_number, expected', [(1234567891011213, '1234 56 ** **** 1213')])
+def test_get_mask_card_number_positive(card_number, expected):
+    assert get_mask_card_number(card_number) == expected
+
+
+@pytest.mark.parametrize('invalid_input', ['1234', ''])
+def test_get_mask_card_number_negative(invalid_input):
+    assert get_mask_card_number(invalid_input)
