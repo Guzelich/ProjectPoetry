55b1ba9c38dd59b80846ce53bc239fe7ce485b69
Guzel Nasybullina <guzelka.nasybullina@gmail.com>
2025-10-06 17:26:06 +0300

    1 test fixed

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -14,7 +14,7 @@ def test_get_mask_account_invalid(invalid_input: str) -> None:
     assert get_mask_account(invalid_input)
 
 
-@pytest.mark.parametrize("card_number, expected", [(1234567891011213, "1234 56 ** **** 1213")])
+@pytest.mark.parametrize("card_number, expected", [(1234567891011213, "1234 56** **** 1213")])
 def test_get_mask_card_number_valid(card_number: int, expected: str) -> None:
     assert get_mask_card_number(card_number) == expected
 
