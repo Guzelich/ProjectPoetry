130de1164f0e9d3af8058b2015c999eaacfb8b90
Guzel Nasybullina <guzelka.nasybullina@gmail.com>
2025-11-02 18:27:37 +0300

    add func filter_by_currency

--- src/generators.py
+++ src/generators.py
@@ -1,1 +1,10 @@
 from typing import Iterator
+
+
+def filter_by_currency(transactions: list, currency: str) -> Iterator:
+    for transaction in transactions:
+        if(
+            isinstance(transaction, dict)
+            and transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency
+        ):
+            yield transaction
