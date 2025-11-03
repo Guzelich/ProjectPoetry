7b93190299400a69ecbda3a33c6413aa452452a9
Guzel Nasybullina <guzelka.nasybullina@gmail.com>
2025-11-03 14:35:10 +0300

    all done

--- src/generators.py
+++ src/generators.py
@@ -1,22 +1,24 @@
 from typing import Iterator
 
 
-def filter_by_currency(transactions: list, currency: str) -> Iterator:
-    for transaction in transactions:
+def filter_by_currency(usd_transactions: list, currency: str) -> Iterator:
+    """Функция принимает на вход список словарей, представляющих транзакции"""
+    for usd_transaction in usd_transactions:
         if (
-            isinstance(transaction, dict)
-            and transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency
+            isinstance(usd_transaction, dict)
+            and usd_transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency
         ):
-            yield transaction
+            yield usd_transaction
 
 
-def transaction_descriptions(transactions: list) -> Iterator:
-    descriptions = transaction_descriptions(transactions)
-    for _ in range(4):
-        yield descriptions
+def transaction_descriptions(usd_transactions: list) -> Iterator:
+    """Генератор принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
+    for usd_transaction in usd_transactions:
+        yield usd_transaction.get("description")
 
 
 def card_number_generator(start: int, end: int) -> Iterator:
+    """Генератор выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где X — цифра номера карты"""
     min_card = 1
     max_card = 9999999999999999
     if start < min_card:
@@ -25,7 +27,6 @@ def card_number_generator(start: int, end: int) -> Iterator:
         raise ValueError("Конечное значение не может быть больше 9999999999999999")
     if start > end:
         raise ValueError("Начальное значение не может быть больше конечного")
-
     for num in range(start, end + 1):
         num_card = str(num).zfill(16)
         yield f"{num_card[:4]} {num_card[4:8]} {num_card[8:12]} {num_card[12:]}"
