d265b6754518be2d03ad6895cf0db82818c32a71
Guzel Nasybullina <guzelka.nasybullina@gmail.com>
2026-01-03 23:25:22 +0300

    after linters

--- src/decorators.py
+++ src/decorators.py
@@ -1,26 +1,29 @@
 import functools
+from typing import Any
 
 
-def log(filename="myfile_log.txt"):
-    def decorator(func):
+def log(filename: str = "myfile_log.txt") -> Any:
+    def decorator(func) -> Any:
         @functools.wraps(func)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
             try:
                 result = func(*args, **kwargs)
-                msg = f'{func.__name__} ok, result: {result}\n'
+                msg = f"{func.__name__} ok, result: {result}\n"
                 _output(msg, filename)
                 return result
             except Exception as e:
-                msg = f'{func.__name__} error: {type(e).__name__}: {e} (args={args}, kwargs={kwargs})\n'
+                msg = f"{func.__name__} error: {type(e).__name__}: {e} (args={args}, kwargs={kwargs})\n"
                 _output(msg, filename)
                 raise
+
         return wrapper
+
     return decorator
 
 
-def _output(msg, filename):
+def _output(msg: Any, filename: Any) -> None:
     if filename:
-        with open(filename, 'a') as f:
+        with open(filename, "a") as f:
             f.write(msg)
     else:
-        print(msg, end='')
+        print(msg, end="")
